5de9f6841a7643ef65f19adfd1ffdde33e2753a3
William Santos <willsantos.edf@gmail.com>
2023-01-04 17:23:38 -0300

    Enviando commit de configurações.

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Dados(models.Model):
+    nome = models.CharField(max_length=50, verbose_name="Nome")
+    email = models.EmailField(verbose_name="Email")
+    telefone = models.CharField(max_length=14, verbose_name="Telefone")
+    cpf = models.CharField(max_length=11, verbose_name='CPF')
+    data_nascimento = models.DateField(verbose_name="Data Nascimento")
+
+    class Meta:
+        verbose_name_plural = "Dados"
+
+    def __str__(self):
+        return f'Dados {self.id}'
+
+
